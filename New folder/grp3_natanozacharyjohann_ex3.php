976ed030a2e53c87106112b6c245534ce45ca5c7
ZacharyJohann <natanozacharyjohann_bsit@plmun.edu.ph>
2024-09-06 20:33:33 +0800

    Update grp3_natanozacharyjohann_ex3.php

--- New folder/grp3_natanozacharyjohann_ex3.php
+++ New folder/grp3_natanozacharyjohann_ex3.php
@@ -1,6 +1,6 @@
 <?php
 
-$filename = 'data.txt';
+$filename = 'text';
 
 if (file_exists($filename)) {
 
@@ -16,7 +16,7 @@ if (file_exists($filename)) {
     }
 } else {
 
-    $content = "Sample data for data.txt file.\nThis file was created using file_put_contents().";
+    $content = "text";
     file_put_contents($filename, $content);
     echo "File '$filename' has been created with the following content:\n";
     echo $content;
